6c48583ae568b04450e2a882f4489ff95e8dc1fc
kelly <kelly.wu@wustl.edu>
2025-09-29 15:35:05 -0500

    mod4 e05 lecture edits

--- src/module4/_02graphics/E05MovingBall.java
+++ src/module4/_02graphics/E05MovingBall.java
@@ -2,7 +2,9 @@ package module4._02graphics;
 
 import java.awt.Color;
 
-import edu.princeton.cs.introcs.StdDraw;
+import java.awt.event.KeyEvent; // import key press events
+
+import edu.princeton.cs.introcs.StdDraw; // API = Application Programming Interface
 
 public class E05MovingBall {
 
@@ -11,9 +13,18 @@ public class E05MovingBall {
 	 * understand how it works before proceeding.
 	 */
 	public static void main(String[] args) {
-		double ballX = 0.0;
+		// starting values
+		double ballX = Math.random();
+		double ballXVelocity = Math.random() * 0.05 + 0.005;
+		double ballY = Math.random();
+		double ballYVelocity = Math.random() * 0.05 + 0.005;
+
+		double playerX = 0.5;
+		double playerY = 0.5;
+		double playerSpeed = 0.025;
+
+		double distanceBetweenPoints = 0.0;
 		
-		StdDraw.setPenColor(Color.RED);
 		StdDraw.setPenRadius(0.05);
 		
 		while (true) {
@@ -22,13 +33,40 @@ public class E05MovingBall {
 			StdDraw.clear();
 			
 			//update the ball position
-			ballX = ballX + 0.005;
-			if(ballX > 1) { //too far right
-				ballX = 0; //back to left
+			// ballX = ballX + 0.005; ==> 0.005 is a magic number so you have to think abt the value it represents. this is bad, use a variable instead
+			ballX = ballX + ballXVelocity;
+			ballY = ballY + ballYVelocity;
+
+			if(ballX > 1 - 0.015 || ballX < 0 + 0.015) { //too far right or left
+				ballXVelocity = -ballXVelocity; //bounce
+			}
+			if(ballY > 1 - 0.015 || ballY < 0 + 0.015) {
+				ballYVelocity = -ballYVelocity;
+			}
+
+			if (StdDraw.isKeyPressed(KeyEvent.VK_W)){ // up
+				playerY += playerSpeed;
+			}
+			if (StdDraw.isKeyPressed(KeyEvent.VK_S)){ // down
+				playerY -= playerSpeed;
+			}
+			if (StdDraw.isKeyPressed(KeyEvent.VK_A)){ // left
+				playerX -= playerSpeed;
+			}
+			if (StdDraw.isKeyPressed(KeyEvent.VK_D)){ // right
+				playerX += playerSpeed;
+			}
+
+			distanceBetweenPoints = Math.sqrt(Math.pow(playerX - ballX, 2) + Math.pow(playerY - ballY, 2));
+			if (distanceBetweenPoints < StdDraw.getPenRadius() * 2){
+				StdDraw.text(0.5, 0.5, "collided");
 			}
 			
 			//draw on canvas
-			StdDraw.point(ballX, 0.5);
+			StdDraw.setPenColor(Color.RED);
+			StdDraw.point(ballX, ballY);
+			StdDraw.setPenColor(Color.BLACK);
+			StdDraw.point(playerX, playerY);
 			
 			//pause to make the animation smooth
 			StdDraw.show();
